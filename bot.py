aa6a70153a3310b49dd35d679bf9764eaf15080f
ao2401043-crypto <ao2401043@gmail.com>
2026-03-08 12:04:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ except ImportError:
     from discord.ext import commands
 
 # --- الإعدادات ---
-TOKEN = 'ضع_التوكن_هنا' 
+TOKEN = 'MTM2MTI2OTYxMTc0Nzc0MTcxNg.GC1mNa.O_CoXgy9IfDsoDCsXOrBXd6o3lx4hTmI7Zt4p0' 
 PREFIX = '!'
 LAST_ROOM_FILE = 'last_room.json'
 
